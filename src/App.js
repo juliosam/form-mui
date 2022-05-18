3d615d76385f5b1d254ff0840d519d507b25c006
juliosam <ja.samaniegom@gmail.com>
2022-05-18 01:02:30 -0500

    create file at 30

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Notes from './pages/Notes'
 import Create from './pages/Create'
+import {createTheme, ThemeProvider} from '@mui/material'
+import { green, purple } from '@mui/material/colors'
+import TextField from '@mui/material/TextField';
+
+const theme = createTheme({
+    palette:{
+      primary:{
+        main: '#fefefe'
+      },
+      secondary: green
+    },
+    typography:{fontFamily:'Quicksand'}
+})
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <Router>
       <Switch>
-        <Route exact path="/">
+        <Route exact path="/"> 
           <Notes />
         </Route>
         <Route path="/create">
@@ -14,6 +28,7 @@ function App() {
         </Route>
       </Switch>
     </Router>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import { Typography } from '@mui/material'
+import React, { useState } from 'react'
+import { TextField, Typography } from '@mui/material'
 import { Button } from '@mui/material'
 import { Container } from '@mui/material'
 import { KeyboardArrowRight } from '@mui/icons-material';
 import { makeStyles } from '@mui/styles'
 
 const useStyles = makeStyles({
-  btn:{
-    fontSize: 60,
-    backgroundColor: 'violet'
+  field:{
+    marginTop:20,
+    marginBottom:20,
+    display:'block'
   }
+ 
 })
 
 export default function Create() {
+  const classes = useStyles();
+  const [title, setTitle] = useState('');
+  const [details, setDetails] = useState('')
   return (
     <div>
       <Container>
@@ -24,13 +29,42 @@ export default function Create() {
       >
         Create a New Note
        </Typography>
+       <form noValidate autoComplete='off'>
+       <TextField
+         onChange={(e)=>setTitle(e.target.value)}
+         className={classes.field}
+         label='Note Title'
+         variant='outlined'
+         color='secondary'
+         fullWidth
+         required
+         />
+         <TextField
+         className={classes.field}
+         label='Details'
+         variant='outlined'
+         color='secondary'
+         multiline
+         rows={4}
+         fullWidth
+         required
+         />
+       </form>
+       <Button
+       variant='contained'
+       color='primary'
+       type='submit'
+       endIcon={<KeyboardArrowRight/>}
+       >
+         Submit
+       </Button>
        <Button
        variant='contained'
        color='secondary'
        type='submit'
        endIcon={<KeyboardArrowRight/>}
        >
-         Submit
+         Purpleter
        </Button>
        </Container>
          
